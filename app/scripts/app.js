c08d5b33b8fcb785fa5c8f15f69b41924319a12c
Gustavo Fernandez <gugutuf@gmail.com>
2014-09-02 22:29:44 -0300

    Ajuste da Demo page com AngularJS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,4 @@
-(function(document) {
-  'use strict';
-
-  document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
-  });
-
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
-})(wrap(document));
+angular.module('polymerApp', [])
+  .controller('polymerController', ['$scope', function($scope) {
+    $scope.width = 500;
+  }]);
